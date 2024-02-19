1799af840bf5ceaa7d3a056331f306d88a9dfd0a
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-19 14:00:06 +0100

    test

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the size of various type
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of long: %zu bytes\n", sizeof(long));
+printf("Size of long long: %zu bytes\n", sizeof(long long));
+printf("Size of float: %zu bytes\n", sizeof(float));
+return (0);
+}
