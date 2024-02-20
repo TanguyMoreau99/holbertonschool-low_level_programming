7c240fa461fb3620b85866ac0a45e5efc4e10a0a
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-20 15:17:24 +0100

    test

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print from 0 to 9
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+		putchar(x);
+		putchar(54)
+		putchar(32)
+	putchar(10);
+
+	return (0);
+}
