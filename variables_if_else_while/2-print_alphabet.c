a3327e03753e539aa317c1a7ed9ab4ceeff5447d
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-20 12:25:39 +0100

    test

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in one line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+putchar("abcdefghijklmnopqrstuvwxyz")
+}
