8441b9cbca70d35577ce01d89563592d499ae7d4
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-20 14:40:45 +0100

    test

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print from 0 to 9
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+		printf(x)
+
+	return (0);
+}
