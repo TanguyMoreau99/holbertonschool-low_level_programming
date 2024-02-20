71f1521b9163c39fa06e070062e7879a79ca06b0
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-20 11:28:15 +0100

    test

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,12 +18,10 @@ int main(void)
 	printf("is");
 		if (n % 10 > 5)
 			printf("and is greater than 5");
+		if (n % 10 < 6)
+			printf("and is less than 6 and not 0");
+		if (n % 10 == 0)
+			printf("and is 0");
 
-			else
-				if (n % 10 < 6)
-					printf("and is less than 6 and not 0");
-
-				if (n % 10 == 0)
-					printf("and is 0");
 	return (0);
 }
