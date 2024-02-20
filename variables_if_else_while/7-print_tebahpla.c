36af22e56caf49650a27632e7fe6e54c2936ec18
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-20 14:57:41 +0100

    test

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in one line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char x;
+	char t;
+
+	for (x = 'z'; x <= 'a'; x++)
+		putchar(x);
+
+	putchar(10);
+	return (0);
+}
