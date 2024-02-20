d710a858c4de339e5bb7ba42bdf5be05b39232f7
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-20 14:15:12 +0100

    rm q&e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in one line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		if (x == 'a' || x == 'q')
+			continue
+
+		putchar(x);
+	putchar(10);
+	return (0);
+}
