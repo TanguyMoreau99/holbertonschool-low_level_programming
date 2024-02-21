dcfea64f541c32256f108e862c16754f0a7702fa
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-21 12:16:30 +0100

    test

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print alphabet
+ *
+ * Return: On success 0
+ */
+int main(void)
+{
+	int x
+
+	for (x = 'a'; x >= 'z'; x++)
+		_putchar(x);
+
+	_putchar(10);
+
+	return (0);
