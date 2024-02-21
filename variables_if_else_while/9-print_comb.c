70be1af428573dc7ba711632d4fa31e43fb76e55
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-21 09:33:18 +0100

    test

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,16 +13,16 @@ int main(void)
 	int x;
 
 	for (x = '0'; x <= '9'; x++)
-
-
+	{
 	putchar(x);
+
 		if (x < 9)
 
-		putchar(44);
-		putchar(32);
+		putchar(',');
+		putchar(' ');
 
 
-
+	}
 	putchar(10);
 
 	return (0);
