e810b36da5bdc1d25ff2fbf3a0709f5c9783b76b
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-23 10:05:31 +0100

    test

---
+++ functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet
+ *
+ * Return: Void
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int t;
+
+	for (t = 1; t <= 10; t++)
+
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+
+	_putchar(10);
+
+}
