90d795a398a31040790bd44fea1a18a20e6cdaa3
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-23 13:16:33 +0100

    test

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * add - check the code
+ *
+ * Return: x
+ */
+int add(int a, int b)
+{
+	return(a + b);
+}
---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * jack_bauer - check the code
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void);
+{
+
+	int m;
+	int h;
+
+	for (h = 0; h <= 23; h++)
+
+}
