f8591a601b600e8288080bf9a4c5aeac490db38e
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-23 11:52:29 +0100

    test

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _abs - x
+ * @n : x
+ * Return: n
+ */
+int _abs(int);
+{
+	int x;
+
+	if (n > 0)
+		return (n);
+
+		else
+			return (-n);
+
+}
