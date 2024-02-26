789fba75bd7053cb33b47bc7effb26257920b5c6
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-26 10:21:56 +0100

    test

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - check
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int t;
+	int x;
+
+	for (t = 1; t <= 10; t++)
+
+		for (x = 0; x <= 14; x++)
+			_putchar(x);
+
+	_putchar(10);
+}
