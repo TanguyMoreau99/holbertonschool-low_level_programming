ce2071591b67684a0fcd6df82be89c5ba9afb6a3
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-26 09:52:29 +0100

    test

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+		if (x == '2' && x == '4')
+			continue;
+	{
+		_putchar(x);
+	}
+
+	_putchar(10);
+}
