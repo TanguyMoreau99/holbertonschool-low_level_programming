75a030fdba790c8b47efca6e21939e87402b6b8b
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-26 10:01:44 +0100

    test

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,15 @@ void print_most_numbers(void)
 	int x;
 
 	for (x = '0'; x <= '9'; x++)
-		if (x == '2' && x == '4')
+		{
+		if (x == '2' || x == '4')
+
 			continue;
+		}
 	{
 		_putchar(x);
 	}
 
 	_putchar(10);
+
 }
