c5d727c9823d686b5ecb4889e362efc3443d86f1
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-26 11:16:47 +0100

    test

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ *
+ * @n : variable
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	for (n >= 0; n)
+		_putchar('_');
+	_putchar(10);
+
+	return (0);
+
+}
