31d04a988508aa6c1b4420ee47638b944a0bf657
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-02-28 11:08:54 +0100

    function that prints an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print the array
+ *
+ * @a : int
+ *
+ * @n : int
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n ; x++)
+	{
+		printf("%d", a[x]);
+	}
+		if (x < n - 1)
+		{
+			printf(", ");
+		}
+	printf("\n");
+}
