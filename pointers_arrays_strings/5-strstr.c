b8aeb6d45da2987f761ae8a84ae1dbf280cc962a
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-03-05 12:55:54 +0100

    add files

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - function
+ *
+ * @haystack : pointerchar
+ * @needle : pointer char
+ *
+ * Return: /
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (haystack);
+	return (needle);
+}
