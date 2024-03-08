585e96943acf908991f08dbff180cb1acff479bf
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-03-08 12:02:52 +0100

    fix error

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,12 +7,10 @@
  *
  * Return: Always 0 (Success)
  */
-
-int main (int argc, char *argv)
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
-	{
 		printf("%s\n", argv[0]);
-	}
+
 	return (0);
 }
