7dc250d959123b086e675b0d1f67975a61b95bec
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-03-08 12:19:41 +0100

    add files

---
+++ argc_argv/4-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point - program that adds positive numbers
+ * @argc: counter of arguments
+ * @argv: variables of arguments
+ *
+ * Return: 0 
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
