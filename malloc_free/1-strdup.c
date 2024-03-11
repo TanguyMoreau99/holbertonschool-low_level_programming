a5b20cdf1831bd56331580083f2200bd4fb4795c
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-03-11 14:32:28 +0100

    fix syntax

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,23 +11,23 @@
 
 char *_strdup(char *str)
 {
-
 	char *sortie;
 	unsigned int index1;
 	unsigned int index2;
 
-	if (str == 0)
-	{
+	if (str == NULL)
 		return (NULL);
-	}
+
 	for (index1 = 0; str[index1] != '\0'; index1++)
+		;
 
 	sortie = (char *)malloc(sizeof(char) * (index1 + 1));
 
 	if (sortie == NULL)
 		return (NULL);
+
 	for (index2 = 0; index2 <= index1; index2++)
-		sortie[index2] = str[index1];
+		sortie[index2] = str[index2];
 
 	return (sortie);
 }
