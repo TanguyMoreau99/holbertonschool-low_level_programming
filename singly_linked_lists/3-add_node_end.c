6cf1d1aa50b5ce87ab1b02494393e8f2592cff02
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-04-03 09:42:47 +0200

    fix code

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,42 +1,59 @@
 #include "lists.h"
+/**
+* _strlen - function that count the length of a string
+* @string: the string that be checked
+*
+* Return: the length of the string
+*/
+int _strlen(const char *string)
+{
+	int length;
+
+	for (length = 0; string[length] != '\0'; length++)
+	{
+	}
+	return (length);
+}
 
 /**
- * add_node_end - add a new node at the end
- * @head: pointer to pointer to the head
- * @str: pointer to the string
- * Return: address of the new element, or NULL if it failed
- */
-
+* *add_node_end - function that adds a new node at the end of a list_t list
+* @head: pointer of pointer to the start of the list
+* @str: a string to add to the beginning of the list
+*
+* Return: the address of the new element, or NULL if it failed
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	char *new_str;
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node; /* Allocate memory space for a new node */
+
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
+	/* Check success of allocation */
 
-	new_str = strdup(str);
+	new_node->str = strdup(str);
+	/* Duplicate string */
 
-	if (new_str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->str = new_str;
-	new_node->len = strlen(str);
-	new_node->next = NULL;
+	new_node->len = _strlen(str);
+	/* Assign the length of the string */
+
+	new_node->next = NULL; /* Set the next of new_node to NULL */
 
 	if (*head == NULL)
-		*head = new_node;
+	{
+		*head = new_node; /* If list is empty, set new_node as the head */
+	}
 	else
 	{
 		list_t *current = *head;
 
-	while (current->next != NULL)
-		current = current->next;
-
-	current->next = new_node;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node; /* Add new_node at the end of the list */
 	}
 
-	return (new_node);
+	return (new_node); /* Return the address of the new node */
 }
